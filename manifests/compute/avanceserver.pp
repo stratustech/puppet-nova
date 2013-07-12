8dbbf662e6f0fa117f443edb13ba7204993a9c0d
Stuiart Stent <stuart.stent@gmail.com>
2013-07-12 09:58:20 -0400

    adding avance.pp template

---
+++ manifests/compute/avanceserver.pp
@@ -0,0 +1,20 @@
+#
+class nova::compute::avanceserver(
+  $avanceapi_connection_url,
+  $avanceapi_connection_username,
+  $avanceapi_connection_password,
+  $avanceapi_inject_image=false
+) {
+
+  nova_config {
+    'DEFAULT/compute_driver':             value => 'avance.AvanceAPIDriver';
+    'DEFAULT/connection_type':            value => 'avanceapi';
+    'DEFAULT/avanceapi_connection_url':      value => $avanceapi_connection_url;
+    'DEFAULT/avanceapi_connection_username': value => $avanceapi_connection_username;
+    'DEFAULT/avanceapi_connection_password': value => $avanceapi_connection_password;
+    'DEFAULT/avanceapi_inject_image':        value => $avanceapi_inject_image;
+  }
+
+##Add extra packages here
+
+}
